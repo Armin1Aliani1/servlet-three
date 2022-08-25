b4527ae48160ae6eacbb6049ee8170fd8082f0bf
Dragon2623 <armin1.aliani1@gmail.com>
2022-08-25 13:35:29 +0430

    add and edit files

---
+++ servletThree/src/main/java/com/servletPractice/ServletOne.java
@@ -0,0 +1,30 @@
+package com.servletPractice;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class ServletOne extends HttpServlet {
+    @Override
+    public void init(ServletConfig config) throws ServletException {
+        super.init(config);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doGet(req, resp);
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
+        out.println("<h3>Test one</h3>");
+        out.close();
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
